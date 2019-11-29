7ae3ab220d2bd0607318f84778a0b864bc2143dc
Inku Xuan <inkuxuan@gmail.com>
2019-11-29 18:23:49 +0800

    bug fix

--- src/team/mai/inku/cartesian/Main.java
+++ src/team/mai/inku/cartesian/Main.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class Main {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String HELP_STR = "BNF Product " + VERSION + " Help" +
             "parameters: [-o <output-file>] [-append] <input-file {input-file}>";
 
